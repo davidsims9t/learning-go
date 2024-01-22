f8660dbc841ddde8a1edbf366bd0d0a52aad98b8
David Sims <davidsims@Davids-MacBook-Air.local>
2024-01-21 17:51:31 -0800

    learning about structs and types

--- main.go
+++ main.go
@@ -2,38 +2,58 @@ package main
 
 import "fmt"
 
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// )
+
+type User struct {
+	id   int
+	name string
+}
+
+type distance float32
+
+func (miles distance) calculateDistance() distance {
+	return miles
+}
+
+func newUser(id int, name string) User {
+	return User{id, name}
+}
+
 // func calculateTax(price float32) (float32, float32) {
 // 	return price * 0.09, price * 0.02
 // }
 
-func birthday(age *int) {
-	if *age > 40 {
-		panic("Dude you're old")
-	}
+// func birthday(age *int) {
+// 	if *age > 40 {
+// 		panic("Dude you're old")
+// 	}
 
-	*age++
-}
+// 	*age++
+// }
 
 func main() {
 	// defer println("Hello World!")
 	// stateTax, cityTax := calculateTax(5)
-	age := 33
-	birthday(&age)
-	println("Age: ", age)
+	// age := 33
+	// birthday(&age)
+	// println("Age: ", age)
 
 	// if message := "Hello World"; age == 34 {
 	// 	println("You are ", age, message)
 	// }
 
-	switch {
-	case age == 33:
-		println("You are 33")
-	default:
-		println("You are ", age)
-	}
+	// switch {
+	// case age == 33:
+	// 	println("You are 33")
+	// default:
+	// 	println("You are ", age)
+	// }
 
-	var collection [10]string
-	collection[0] = "Test"
+	// var collection [10]string
+	// collection[0] = "Test"
 
 	// for i := 0; i < len(collection); i++ {
 	// 	println(collection[i])
@@ -50,13 +70,26 @@ func main() {
 	// }
 
 	// var num1, num2 int
-	var operation string
+	// var operation string
 
-	fmt.Println("Enter an operation")
-	fmt.Scanf("%s", &operation)
+	// fmt.Println("Enter an operation")
+	// fmt.Scanf("%s", &operation)
 
-	switch operation {
-	case "add":
-		println("You are adding")
-	}
+	// switch operation {
+	// case "add":
+	// 	println("You are adding")
+	// }
+
+	// content, err := ioutil.ReadFile("./data.txt")
+
+	// if err != nil {
+	// } else {
+	// 	fmt.Println(string(content))
+	// }
+
+	// km := distance(5.12)
+	// fmt.Println("The distance is ", km)
+
+	user := newUser(1, "Bob")
+	fmt.Println("User id is", user.id, "and name is", user.name)
 }
